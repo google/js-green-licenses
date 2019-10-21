fb976024109f9814e2819415c414f2eb397d2679
Justin Beckwith <beckwith@google.com>
2019-10-21 13:18:46 -0700

    refactor: use mocha and c8 for tests (#107)

--- test/config-test.ts
+++ test/config-test.ts
@@ -12,166 +12,176 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import test from 'ava';
+import * as assert from 'assert';
 import { withFixtures } from 'inline-fixtures';
-
 import * as config from '../src/config';
 import { GitHubRepository } from '../src/github';
 
-test.serial('read correct contents from local config file', t => {
-  const configContent = JSON.stringify({
-    greenLicenses: ['FOO', 'BAR'],
-    packageWhitelist: ['a-package', 'another-package'],
-  });
+describe(__filename, () => {
+  it('read correct contents from local config file', async () => {
+    const configContent = JSON.stringify({
+      greenLicenses: ['FOO', 'BAR'],
+      packageWhitelist: ['a-package', 'another-package'],
+    });
 
-  return withFixtures(
-    { 'repo/directory': { 'js-green-licenses.json': configContent } },
-    async () => {
-      const cfg = await config.getLocalConfig('repo/directory');
-      t.truthy(cfg);
-      t.deepEqual(cfg!.greenLicenses, ['FOO', 'BAR']);
-      t.deepEqual(cfg!.packageWhitelist, ['a-package', 'another-package']);
-    }
-  );
-});
-
-test.serial('read config file from github repo', async t => {
-  class FakeGitHubRepository extends GitHubRepository {
-    constructor() {
-      super('janedoe', 'repo');
-    }
-
-    async getFileContent(
-      commitSha: string,
-      path: string
-    ): Promise<string | null> {
-      return JSON.stringify({
-        greenLicenses: ['BAR', 'BAZ'],
-        packageWhitelist: ['package-1', 'package-2'],
-      });
-    }
-  }
-  const cfg = await config.getGitHubConfig(new FakeGitHubRepository(), '1234');
-  t.truthy(cfg);
-  t.deepEqual(cfg!.greenLicenses, ['BAR', 'BAZ']);
-  t.deepEqual(cfg!.packageWhitelist, ['package-1', 'package-2']);
-});
-
-test.serial('no config file is ok (local)', t => {
-  return withFixtures({ 'repo/directory': {} }, async () => {
-    const cfg = await config.getLocalConfig('repo/directory');
-    t.is(cfg, null);
-  });
-});
-
-test.serial('no config file is ok (github)', async t => {
-  class FakeGitHubRepository extends GitHubRepository {
-    constructor() {
-      super('janedoe', 'repo');
-    }
-
-    async getFileContent(
-      commitSha: string,
-      path: string
-    ): Promise<string | null> {
-      return null;
-    }
-  }
-  const cfg = await config.getGitHubConfig(new FakeGitHubRepository(), '1234');
-  t.is(cfg, null);
-});
-
-test.serial('error for invalid config file (local)', t => {
-  const configContent = JSON.stringify({
-    // must be an array of strings.
-    packageWhitelist: [42, 43],
-  });
-  return withFixtures(
-    {
-      'repo/directory': {
-        'js-green-licenses.json': configContent,
-      },
-    },
-    async () => {
-      const consoleError = console.error;
-      try {
-        let errorContents = '';
-        console.error = (message, ...params) => {
-          errorContents = `${message} ${params.join(' ')}`;
-          consoleError(message, ...params);
-        };
+    return withFixtures(
+      { 'repo/directory': { 'js-green-licenses.json': configContent } },
+      async () => {
         const cfg = await config.getLocalConfig('repo/directory');
-        t.is(cfg, null);
-        t.true(errorContents.indexOf('Invalid config contents') >= 0);
-      } finally {
-        console.error = consoleError;
+        assert.ok(cfg);
+        assert.deepStrictEqual(cfg!.greenLicenses, ['FOO', 'BAR']);
+        assert.deepStrictEqual(cfg!.packageWhitelist, [
+          'a-package',
+          'another-package',
+        ]);
+      }
+    );
+  });
+
+  it('read config file from github repo', async () => {
+    class FakeGitHubRepository extends GitHubRepository {
+      constructor() {
+        super('janedoe', 'repo');
+      }
+
+      async getFileContent(
+        commitSha: string,
+        path: string
+      ): Promise<string | null> {
+        return JSON.stringify({
+          greenLicenses: ['BAR', 'BAZ'],
+          packageWhitelist: ['package-1', 'package-2'],
+        });
       }
     }
-  );
-});
-
-test.serial('error for invalid config file (github)', async t => {
-  class FakeGitHubRepository extends GitHubRepository {
-    constructor() {
-      super('janedoe', 'repo');
-    }
-
-    async getFileContent(
-      commitSha: string,
-      path: string
-    ): Promise<string | null> {
-      return JSON.stringify({
-        // must be an array.
-        greenLicenses: {
-          FOO: '^1.2.3',
-          BAR: '^2.3.4',
-        },
-      });
-    }
-  }
-
-  const consoleError = console.error;
-  try {
-    let errorContents = '';
-    console.error = (message, ...params) => {
-      errorContents = `${message} ${params.join(' ')}`;
-      consoleError(message, ...params);
-    };
     const cfg = await config.getGitHubConfig(
       new FakeGitHubRepository(),
       '1234'
     );
-    t.is(cfg, null);
-    t.true(errorContents.indexOf('Invalid config contents') >= 0);
-  } finally {
-    console.error = consoleError;
-  }
-});
+    assert.ok(cfg);
+    assert.deepStrictEqual(cfg!.greenLicenses, ['BAR', 'BAZ']);
+    assert.deepStrictEqual(cfg!.packageWhitelist, ['package-1', 'package-2']);
+  });
 
-test.serial('comments are allowed in config file', t => {
-  const configContent = `{
-    // comments are fine
-    "greenLicenses": [
-      "FOO",
-      "BAR"
-    ],
-    /* another form of comment */
-    "packageWhitelist": [
-      "foo",  // inline comment
-      "bar"
-    ]
-  }`;
-  return withFixtures(
-    {
-      'repo/directory': {
-        'js-green-licenses.json': configContent,
-      },
-    },
-    async () => {
+  it('no config file is ok (local)', async () => {
+    return withFixtures({ 'repo/directory': {} }, async () => {
       const cfg = await config.getLocalConfig('repo/directory');
-      t.truthy(cfg);
-      t.deepEqual(cfg!.greenLicenses, ['FOO', 'BAR']);
-      t.deepEqual(cfg!.packageWhitelist, ['foo', 'bar']);
+      assert.strictEqual(cfg, null);
+    });
+  });
+
+  it('no config file is ok (github)', async () => {
+    class FakeGitHubRepository extends GitHubRepository {
+      constructor() {
+        super('janedoe', 'repo');
+      }
+
+      async getFileContent(
+        commitSha: string,
+        path: string
+      ): Promise<string | null> {
+        return null;
+      }
     }
-  );
+    const cfg = await config.getGitHubConfig(
+      new FakeGitHubRepository(),
+      '1234'
+    );
+    assert.strictEqual(cfg, null);
+  });
+
+  it('error for invalid config file (local)', async () => {
+    const configContent = JSON.stringify({
+      // must be an array of strings.
+      packageWhitelist: [42, 43],
+    });
+    return withFixtures(
+      {
+        'repo/directory': {
+          'js-green-licenses.json': configContent,
+        },
+      },
+      async () => {
+        const consoleError = console.error;
+        try {
+          let errorContents = '';
+          console.error = (message, ...params) => {
+            errorContents = `${message} ${params.join(' ')}`;
+            consoleError(message, ...params);
+          };
+          const cfg = await config.getLocalConfig('repo/directory');
+          assert.strictEqual(cfg, null);
+          assert.ok(errorContents.indexOf('Invalid config contents') >= 0);
+        } finally {
+          console.error = consoleError;
+        }
+      }
+    );
+  });
+
+  it('error for invalid config file (github)', async () => {
+    class FakeGitHubRepository extends GitHubRepository {
+      constructor() {
+        super('janedoe', 'repo');
+      }
+
+      async getFileContent(
+        commitSha: string,
+        path: string
+      ): Promise<string | null> {
+        return JSON.stringify({
+          // must be an array.
+          greenLicenses: {
+            FOO: '^1.2.3',
+            BAR: '^2.3.4',
+          },
+        });
+      }
+    }
+
+    const consoleError = console.error;
+    try {
+      let errorContents = '';
+      console.error = (message, ...params) => {
+        errorContents = `${message} ${params.join(' ')}`;
+        consoleError(message, ...params);
+      };
+      const cfg = await config.getGitHubConfig(
+        new FakeGitHubRepository(),
+        '1234'
+      );
+      assert.strictEqual(cfg, null);
+      assert.ok(errorContents.indexOf('Invalid config contents') >= 0);
+    } finally {
+      console.error = consoleError;
+    }
+  });
+
+  it('comments are allowed in config file', async () => {
+    const configContent = `{
+      // comments are fine
+      "greenLicenses": [
+        "FOO",
+        "BAR"
+      ],
+      /* another form of comment */
+      "packageWhitelist": [
+        "foo",  // inline comment
+        "bar"
+      ]
+    }`;
+    return withFixtures(
+      {
+        'repo/directory': {
+          'js-green-licenses.json': configContent,
+        },
+      },
+      async () => {
+        const cfg = await config.getLocalConfig('repo/directory');
+        assert.ok(cfg);
+        assert.deepStrictEqual(cfg!.greenLicenses, ['FOO', 'BAR']);
+        assert.deepStrictEqual(cfg!.packageWhitelist, ['foo', 'bar']);
+      }
+    );
+  });
 });
--- test/github-test.ts
+++ test/github-test.ts
@@ -12,51 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import test from 'ava';
+import * as assert from 'assert';
 import nock from 'nock';
+import assertRejects = require('assert-rejects');
 import { GitHubRepository } from '../src/github';
 
-interface WithRepo {
-  repo: GitHubRepository;
-}
-
-function contextualize<T>(getContext: () => T) {
-  test.beforeEach(t => {
-    Object.assign(t.context as WithRepo, getContext());
+describe(__filename, () => {
+  let repo: GitHubRepository;
+  beforeEach(() => {
+    repo = new GitHubRepository('luke', 'star-destroyer');
   });
-  return test;
-}
-const testContextual = contextualize<WithRepo>(() => ({
-  repo: new GitHubRepository('luke', 'star-destroyer'),
-}));
 
-testContextual.afterEach.always('cleanup', t => {
-  nock.cleanAll();
-});
+  afterEach(() => nock.cleanAll());
 
-// NOTE: Tests must be run serially so that nock interceptions in each test
-// don't interfere with other tests.
+  it('should correctly get PR commit SHAs', async () => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/luke/star-destroyer/pulls/12345')
+      .reply(200, {
+        mergeable: true,
+        merge_commit_sha: 'deadbeef',
+        head: { sha: 'foobar' },
+      });
+    const { mergeCommitSha, headCommitSha } = await repo.getPRCommits(12345);
+    scope.done();
+    assert.strictEqual(mergeCommitSha, 'deadbeef');
+    assert.strictEqual(headCommitSha, 'foobar');
+  });
 
-testContextual.serial('should correctly get PR commit SHAs', async t => {
-  const scope = nock('https://api.github.com')
-    .get('/repos/luke/star-destroyer/pulls/12345')
-    .reply(200, {
-      mergeable: true,
-      merge_commit_sha: 'deadbeef',
-      head: { sha: 'foobar' },
-    });
-  const {
-    mergeCommitSha,
-    headCommitSha,
-  } = await (t.context as WithRepo).repo.getPRCommits(12345);
-  scope.done();
-  t.is(mergeCommitSha, 'deadbeef');
-  t.is(headCommitSha, 'foobar');
-});
-
-testContextual.serial(
-  'should retry when unknown whether PR is mergeable',
-  async t => {
+  it('should retry when unknown whether PR is mergeable', async () => {
     const firstScope = nock('https://api.github.com')
       .get('/repos/luke/star-destroyer/pulls/12345')
       .reply(200, {
@@ -69,145 +52,112 @@ testContextual.serial(
         merge_commit_sha: 'deadbeef',
         head: { sha: 'foobar' },
       });
-    const {
-      mergeCommitSha,
-      headCommitSha,
-    } = await (t.context as WithRepo).repo.getPRCommits(12345);
+    const { mergeCommitSha, headCommitSha } = await repo.getPRCommits(12345);
     firstScope.done();
     retryScope.done();
-    t.is(mergeCommitSha, 'deadbeef');
-    t.is(headCommitSha, 'foobar');
-  }
-);
+    assert.strictEqual(mergeCommitSha, 'deadbeef');
+    assert.strictEqual(headCommitSha, 'foobar');
+  });
 
-testContextual.serial('should throw on max retries', async t => {
-  const scope = nock('https://api.github.com')
-    .get('/repos/luke/star-destroyer/pulls/12345')
-    .reply(200, {
-      mergeable: null,
-    });
-  // Currently gives up after 10 retries (i.e. 11 tries total).
-  const err: Error = await t.throwsAsync(
-    (t.context as WithRepo).repo.getPRCommits(12345, 11)
-  );
-  scope.done();
-  t.is(
-    err.message,
-    'Tried 11 times but the mergeable field is not set. Giving up'
-  );
-});
+  it('should throw on max retries', async () => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/luke/star-destroyer/pulls/12345')
+      .reply(200, {
+        mergeable: null,
+      });
+    // Currently gives up after 10 retries (i.e. 11 tries total).
+    await assertRejects(
+      repo.getPRCommits(12345, 11),
+      /Tried 11 times but the mergeable field is not set. Giving up/
+    );
+    scope.done();
+  });
 
-testContextual.serial('should throw when PR is not mergeable', async t => {
-  const scope = nock('https://api.github.com')
-    .get('/repos/luke/star-destroyer/pulls/12345')
-    .reply(200, {
-      mergeable: false,
-      merge_commit_sha: 'deadbeef',
-      head: { sha: 'foobar' },
-    });
-  const err: Error = await t.throwsAsync(
-    (t.context as WithRepo).repo.getPRCommits(12345),
-    Error
-  );
-  scope.done();
-  t.is(err.message, 'PR is not mergeable');
-});
+  it('should throw when PR is not mergeable', async () => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/luke/star-destroyer/pulls/12345')
+      .reply(200, {
+        mergeable: false,
+        merge_commit_sha: 'deadbeef',
+        head: { sha: 'foobar' },
+      });
+    await assertRejects(repo.getPRCommits(12345), /PR is not mergeable/);
+    scope.done();
+  });
 
-testContextual.serial(
-  'should throw when PR merge commit sha is not found',
-  async t => {
+  it('should throw when PR merge commit sha is not found', async () => {
     const scope = nock('https://api.github.com')
       .get('/repos/luke/star-destroyer/pulls/12345')
       .reply(200, {
         mergeable: true,
         head: { sha: 'foobar' },
       });
-    const err: Error = await t.throwsAsync(
-      (t.context as WithRepo).repo.getPRCommits(12345),
-      Error
+    await assertRejects(
+      repo.getPRCommits(12345),
+      /Merge commit SHA is not found/
     );
     scope.done();
-    t.is(err.message, 'Merge commit SHA is not found');
-  }
-);
+  });
 
-testContextual.serial(
-  'should throw when PR HEAD commit sha is not found',
-  async t => {
+  it('should throw when PR HEAD commit sha is not found', async () => {
     const scope = nock('https://api.github.com')
       .get('/repos/luke/star-destroyer/pulls/12345')
       .reply(200, {
         mergeable: true,
         merge_commit_sha: 'deadbeef',
       });
-    const err: Error = await t.throwsAsync(
-      (t.context as WithRepo).repo.getPRCommits(12345),
-      Error
+    await assertRejects(
+      repo.getPRCommits(12345),
+      /HEAD commit SHA is not found/
     );
     scope.done();
-    t.is(err.message, 'HEAD commit SHA is not found');
-  }
-);
+  });
 
-testContextual.serial('retrieve file content', async t => {
-  const scope = nock('https://api.github.com')
-    .get('/repos/luke/star-destroyer/contents/src/foo.js?ref=deadbeef')
-    .reply(200, {
-      content: Buffer.from('this is foo.js').toString('base64'),
-    });
-  const content = await (t.context as WithRepo).repo.getFileContent(
-    'deadbeef',
-    'src/foo.js'
-  );
-  scope.done();
-  t.is(content, 'this is foo.js');
-});
+  it('retrieve file content', async () => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/luke/star-destroyer/contents/src/foo.js?ref=deadbeef')
+      .reply(200, {
+        content: Buffer.from('this is foo.js').toString('base64'),
+      });
+    const content = await repo.getFileContent('deadbeef', 'src/foo.js');
+    scope.done();
+    assert.strictEqual(content, 'this is foo.js');
+  });
 
-testContextual.serial(
-  'should retrieve package.json from a single repo',
-  async t => {
+  it('should retrieve package.json from a single repo', async () => {
     const scope = nock('https://api.github.com')
       .get('/repos/luke/star-destroyer/contents/package.json?ref=deadbeef')
       .reply(200, {
         content: Buffer.from('this is package.json').toString('base64'),
       });
-    const pj = await (t.context as WithRepo).repo.getPackageJsonFiles(
-      'deadbeef'
-    );
+    const pj = await repo.getPackageJsonFiles('deadbeef');
     scope.done();
-    t.is(pj.length, 1);
-    t.is(pj[0].filePath, '/package.json');
-    t.is(pj[0].content, 'this is package.json');
-  }
-);
+    assert.strictEqual(pj.length, 1);
+    assert.strictEqual(pj[0].filePath, '/package.json');
+    assert.strictEqual(pj[0].content, 'this is package.json');
+  });
 
-testContextual.serial('should handle no package.json files', async t => {
-  const scope = nock('https://api.github.com')
-    .get('/repos/luke/star-destroyer/contents/package.json?ref=deadbeef')
-    .reply(404);
-  const pj = await (t.context as WithRepo).repo.getPackageJsonFiles('deadbeef');
-  scope.done();
-  t.is(pj.length, 0);
-});
+  it('should handle no package.json files', async () => {
+    const scope = nock('https://api.github.com')
+      .get('/repos/luke/star-destroyer/contents/package.json?ref=deadbeef')
+      .reply(404);
+    const pj = await repo.getPackageJsonFiles('deadbeef');
+    scope.done();
+    assert.strictEqual(pj.length, 0);
+  });
 
-testContextual.serial(
-  'should throw when package.json does not have contents',
-  async t => {
+  it('should throw when package.json does not have contents', async () => {
     const scope = nock('https://api.github.com')
       .get('/repos/luke/star-destroyer/contents/package.json' + '?ref=deadbeef')
       .reply(200);
-    const err: Error = await t.throwsAsync(
-      (t.context as WithRepo).repo.getPackageJsonFiles('deadbeef'),
-      Error
+    await assertRejects(
+      repo.getPackageJsonFiles('deadbeef'),
+      /Content of package.json not found/
     );
     scope.done();
-    t.is(err.message, 'Content of package.json not found');
-  }
-);
+  });
 
-testContextual.serial(
-  'should get all package.json files from monorepo',
-  async t => {
+  it('should get all package.json files from monorepo', async () => {
     const topScope = nock('https://api.github.com')
       .get('/repos/luke/star-destroyer/contents/package.json?ref=deadbeef')
       .reply(200, {
@@ -245,19 +195,17 @@ testContextual.serial(
       .reply(200, {
         content: Buffer.from('baz package.json').toString('base64'),
       });
-    const pj = await (t.context as WithRepo).repo.getPackageJsonFiles(
-      'deadbeef'
-    );
+    const pj = await repo.getPackageJsonFiles('deadbeef');
     topScope.done();
     packagesScope.done();
     fooScope.done();
     bazScope.done();
-    t.is(pj.length, 3);
-    t.is(pj[0].filePath, '/package.json');
-    t.is(pj[0].content, 'top-level package.json');
-    t.is(pj[1].filePath, '/packages/foo/package.json');
-    t.is(pj[1].content, 'foo package.json');
-    t.is(pj[2].filePath, '/packages/baz/package.json');
-    t.is(pj[2].content, 'baz package.json');
-  }
-);
+    assert.strictEqual(pj.length, 3);
+    assert.strictEqual(pj[0].filePath, '/package.json');
+    assert.strictEqual(pj[0].content, 'top-level package.json');
+    assert.strictEqual(pj[1].filePath, '/packages/foo/package.json');
+    assert.strictEqual(pj[1].content, 'foo package.json');
+    assert.strictEqual(pj[2].filePath, '/packages/baz/package.json');
+    assert.strictEqual(pj[2].content, 'baz package.json');
+  });
+});
