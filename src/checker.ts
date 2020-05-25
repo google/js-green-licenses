0d95d18eb146bb8a003e741789729cf88d7e759c
Mat Sz <contact@matsz.dev>
2020-05-26 01:54:55 +0200

    feat: added skipping of package.json check if package is whitelisted (resolves #118) (#136)

--- src/checker.ts
+++ src/checker.ts
@@ -20,6 +20,7 @@ import * as path from 'path';
 import spdxCorrect from 'spdx-correct';
 import spdxSatisfies from 'spdx-satisfies';
 import {inspect, promisify} from 'util';
+import semver from 'semver';
 
 import * as config from './config';
 import {GitHubRepository} from './github';
@@ -175,7 +176,7 @@ export class LicenseChecker extends EventEmitter {
     this.failedPackages.clear();
   }
 
-  private getLicense(pkgJson: PackageJson): string | null {
+  private getLicense(pkgJson: Partial<PackageJson>): string | null {
     // Some package.json files have incorrect license fields, and old packages
     // may have legacy licence field format. See
     // https://docs.npmjs.com/files/package.json#license for details. The code
@@ -310,21 +311,27 @@ export class LicenseChecker extends EventEmitter {
   }
 
   private async checkPackageJson(
-    json: {},
+    json: Partial<PackageJson>,
     packageName: string | null,
     localDirectory: string | null,
     ...parents: string[]
   ): Promise<void> {
-    const pj: PackageJson = ensurePackageJson(json);
-    if (!packageName) {
-      packageName = pj.name;
+    packageName = (packageName || json.name || 'undefined') as string;
+
+    const isWhitelisted = this.isPackageWhitelisted(packageName);
+    if (isWhitelisted) {
+      json.version = semver.valid(json.version) ? json.version : '0.0.0';
+    } else {
+      ensurePackageJson(json);
     }
-    if (pj.name !== packageName) {
+
+    if (json.name !== packageName) {
       console.warn(
-        `Package name mismatch. Expected ${packageName}, but got ${pj.name}`
+        `Package name mismatch. Expected ${packageName}, but got ${json.name}`
       );
     }
-    const pkgVersion = pj.version;
+
+    const pkgVersion = json.version;
     const packageAndVersion = `${packageName}@${pkgVersion}`;
     if (this.processedPackages.has(packageAndVersion)) return;
     this.processedPackages.add(packageAndVersion);
@@ -332,11 +339,11 @@ export class LicenseChecker extends EventEmitter {
     if (this.isPackageWhitelisted(packageName)) {
       console.log(`${packageName} is whitelisted.`);
     } else {
-      const license = this.getLicense(pj);
+      const license = this.getLicense(json);
       if (!this.isGreenLicense(license)) {
         this.emit('non-green-license', {
           packageName,
-          version: pkgVersion,
+          version: pkgVersion || 'undefined',
           licenseName: license,
           parentPackages: parents,
         });
@@ -344,14 +351,14 @@ export class LicenseChecker extends EventEmitter {
     }
 
     await this.checkLicensesForDeps(
-      pj.dependencies,
+      json.dependencies,
       localDirectory,
       ...parents,
       packageAndVersion
     );
     if (this.opts.dev) {
       await this.checkLicensesForDeps(
-        pj.devDependencies,
+        json.devDependencies,
         localDirectory,
         ...parents,
         packageAndVersion
